a27f8073fadbaef25bce67579d0317742a3c0251
darlynee zamorano <darl.zamorano@duocuc.cl>
2025-07-02 00:07:20 -0400

    creacion

--- src/main/java/com/perfulandia/vendedores/controller/VendedoresController.java
+++ src/main/java/com/perfulandia/vendedores/controller/VendedoresController.java
@@ -1,13 +1,15 @@
 package com.perfulandia.vendedores.controller;
 
-
 import com.perfulandia.vendedores.dto.VendedoresDTO;
 import com.perfulandia.vendedores.services.Vendedoresservices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/vendedores")
@@ -18,22 +20,42 @@ public class VendedoresController {
 
     @PostMapping
     public ResponseEntity<VendedoresDTO> crear(@RequestBody VendedoresDTO dto) {
-        return ResponseEntity.ok(service.crear(dto));
+        VendedoresDTO creado = service.crear(dto);
+        creado.add(linkTo(methodOn(VendedoresController.class).obtener(creado.getId())).withSelfRel());
+        creado.add(linkTo(methodOn(VendedoresController.class).listar()).withRel("todos-los-vendedores"));
+        return ResponseEntity.ok(creado);
     }
 
     @GetMapping
-    public ResponseEntity<List<VendedoresDTO>> listar() {
-        return ResponseEntity.ok(service.listar());
+    public ResponseEntity<CollectionModel<VendedoresDTO>> listar() {
+        List<VendedoresDTO> lista = service.listar();
+
+        lista.forEach(v ->
+            v.add(linkTo(methodOn(VendedoresController.class).obtener(v.getId())).withSelfRel())
+        );
+
+        CollectionModel<VendedoresDTO> modelo = CollectionModel.of(lista);
+        modelo.add(linkTo(methodOn(VendedoresController.class).listar()).withSelfRel());
+
+        return ResponseEntity.ok(modelo);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<VendedoresDTO> obtener(@PathVariable Integer id) {
-        return ResponseEntity.ok(service.obtenerPorId(id));
+        VendedoresDTO vendedor = service.obtenerPorId(id);
+
+        vendedor.add(linkTo(methodOn(VendedoresController.class).obtener(id)).withSelfRel());
+        vendedor.add(linkTo(methodOn(VendedoresController.class).listar()).withRel("todos-los-vendedores"));
+
+        return ResponseEntity.ok(vendedor);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<VendedoresDTO> actualizar(@PathVariable Integer id, @RequestBody VendedoresDTO dto) {
-        return ResponseEntity.ok(service.actualizar(id, dto));
+        VendedoresDTO actualizado = service.actualizar(id, dto);
+        actualizado.add(linkTo(methodOn(VendedoresController.class).obtener(id)).withSelfRel());
+        actualizado.add(linkTo(methodOn(VendedoresController.class).listar()).withRel("todos-los-vendedores"));
+        return ResponseEntity.ok(actualizado);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/perfulandia/vendedores/dto/VendedoresDTO.java
+++ src/main/java/com/perfulandia/vendedores/dto/VendedoresDTO.java
@@ -1,10 +1,12 @@
 package com.perfulandia.vendedores.dto;
 
+import org.springframework.hateoas.RepresentationModel;
 import lombok.Data;
 
 @Data
-public class VendedoresDTO {
+public class VendedoresDTO extends RepresentationModel<VendedoresDTO>{
     private Integer id;
     private String sucursal;
     private Double metaMensual;
-}
+} 
+
